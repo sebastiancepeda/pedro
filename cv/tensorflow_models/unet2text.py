79d309e6c60880cee28d0b19276cabc721455142
Sebastian Cepeda Fuentealba <sebastian.cepeda.fuentealba@gmail.com>
2020-08-01 22:52:34 -0400

    Adding dropout to ocr model

--- cv/tensorflow_models/unet2text.py
+++ cv/tensorflow_models/unet2text.py
@@ -5,7 +5,8 @@
 
 from tensorflow.keras import Model
 from tensorflow.keras.layers import (
-    Conv2D, Conv2DTranspose, Lambda, Input,
+    Conv2D, Conv2DTranspose,
+    Dropout, Lambda, Input,
     MaxPooling2D, concatenate,
 )
 
@@ -34,6 +35,7 @@ def get_model_definition(img_height, img_width, in_channels, out_channels):
         'kernel_initializer': 'he_normal',
         'padding': 'same',
     }
+    drop_p = 0.1
     h_dim = 100  # 100
     outs = {
         1: h_dim,  # 64
@@ -45,35 +47,44 @@ def get_model_definition(img_height, img_width, in_channels, out_channels):
     # Down
     c1 = Conv2D(outs[1], **kwargs_conv2d)(pre_processing)
     c1 = Conv2D(outs[1], **kwargs_conv2d)(c1)
+    c1 = Dropout(drop_p)(c1)
     c2 = MaxPooling2D((2, 2))(c1)
     c2 = Conv2D(outs[2], **kwargs_conv2d)(c2)
     c2 = Conv2D(outs[2], **kwargs_conv2d)(c2)
+    c2 = Dropout(drop_p)(c2)
     c3 = MaxPooling2D((2, 2))(c2)
     c3 = Conv2D(outs[3], **kwargs_conv2d)(c3)
     c3 = Conv2D(outs[3], **kwargs_conv2d)(c3)
+    c3 = Dropout(drop_p)(c3)
     c4 = MaxPooling2D((2, 2))(c3)
     c4 = Conv2D(outs[4], **kwargs_conv2d)(c4)
     c4 = Conv2D(outs[4], **kwargs_conv2d)(c4)
+    c4 = Dropout(drop_p)(c4)
     c5 = MaxPooling2D(pool_size=(2, 2))(c4)
     c5 = Conv2D(outs[5], **kwargs_conv2d)(c5)
     c5 = Conv2D(outs[5], **kwargs_conv2d)(c5)
+    c5 = Dropout(drop_p)(c5)
     # Up
     u4 = Conv2DTranspose(outs[4], (2, 2), strides=(2, 2), padding='same')(c5)
     u4 = concatenate([u4, c4])
     u4 = Conv2D(outs[4], **kwargs_conv2d)(u4)
     u4 = Conv2D(outs[4], **kwargs_conv2d)(u4)
+    u4 = Dropout(drop_p)(u4)
     u3 = Conv2DTranspose(outs[3], (2, 2), strides=(2, 2), padding='same')(u4)
     u3 = concatenate([u3, c3])
     u3 = Conv2D(outs[3], **kwargs_conv2d)(u3)
     u3 = Conv2D(outs[3], **kwargs_conv2d)(u3)
+    u3 = Dropout(drop_p)(u3)
     u2 = Conv2DTranspose(outs[2], (2, 2), strides=(2, 2), padding='same')(u3)
     u2 = concatenate([u2, c2])
     u2 = Conv2D(outs[2], **kwargs_conv2d)(u2)
     u2 = Conv2D(outs[2], **kwargs_conv2d)(u2)
+    u2 = Dropout(drop_p)(u2)
     u1 = Conv2DTranspose(outs[1], (2, 2), strides=(2, 2), padding='same')(u2)
     u1 = concatenate([u1, c1], axis=3)
     u1 = Conv2D(outs[1], **kwargs_conv2d)(u1)
     u1 = Conv2D(outs[1], **kwargs_conv2d)(u1)
+    u1 = Dropout(drop_p)(u1)
     # Downward
     kwargs_conv2d = {
         'activation': 'relu',
@@ -81,16 +92,22 @@ def get_model_definition(img_height, img_width, in_channels, out_channels):
         'padding': 'same',
     }
     x = Conv2D(h_dim, kernel_size=(3, 3), **kwargs_conv2d)(u1)
+    x = Dropout(drop_p)(x)
     x = MaxPooling2D((2, 2))(x)
     x = Conv2D(h_dim, kernel_size=(3, 3), **kwargs_conv2d)(x)
+    x = Dropout(drop_p)(x)
     x = MaxPooling2D((2, 2))(x)
     x = Conv2D(h_dim, kernel_size=(3, 3), **kwargs_conv2d)(x)
+    x = Dropout(drop_p)(x)
     x = MaxPooling2D((2, 2))(x)
     x = Conv2D(h_dim, kernel_size=(3, 3), **kwargs_conv2d)(x)
+    x = Dropout(drop_p)(x)
     x = MaxPooling2D((2, 2))(x)
     x = Conv2D(h_dim, kernel_size=(3, 3), **kwargs_conv2d)(x)
+    x = Dropout(drop_p)(x)
     x = MaxPooling2D((4, 1))(x)
     x = Conv2D(h_dim, kernel_size=(1, 3), **kwargs_conv2d)(x)
+    x = Dropout(drop_p)(x)
     outputs = Conv2D(out_channels, (1, 1), activation='sigmoid')(x)
     # Model compilation
     model = Model(inputs=[inputs], outputs=[outputs])
