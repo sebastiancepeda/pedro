145f95317ce7b38e737fb9824ae85e76d6652584
Sebastian Cepeda Fuentealba <sebastian.cepeda.fuentealba@gmail.com>
2020-08-20 23:38:08 -0400

    Bigger net

--- services/plate_ocr/training.py
+++ services/plate_ocr/training.py
@@ -29,7 +29,7 @@ def get_params():
     params = {
         'input_folder': input_folder,
         'output_folder': output_folder,
-        'epochs': 1 * 1000,
+        'epochs': 10 * 1000,
         'dsize': dsize,
         'model_folder': f'{output_folder}/model',
         'model_file': f'{output_folder}/model/best_model.h5',
