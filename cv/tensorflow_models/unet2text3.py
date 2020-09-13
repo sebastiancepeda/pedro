4ee4f145fc86082a1730fd105b781efa04d3bdeb
Sebastian Cepeda Fuentealba <sebastian.cepeda.fuentealba@gmail.com>
2020-09-13 12:29:35 -0300

    max pooling

--- cv/tensorflow_models/unet2text3.py
+++ cv/tensorflow_models/unet2text3.py
@@ -36,8 +36,8 @@ def get_model_definition(img_height, img_width, in_channels, out_channels):
     msg = "{actual} not multiple of " + str(base)
     assert img_height % base == 0, msg.format(actual=img_height)
     assert img_width % base == 0, msg.format(actual=img_width)
-    inputs = Input((img_height, img_width, in_channels))
-    x = Lambda(lambda aux: aux / 255)(inputs)
+    input_image = Input((img_height, img_width, in_channels))
+    x = Lambda(lambda aux: aux / 255)(input_image)
     # Downward
     kwargs_conv2d = {
         'activation': 'relu',
@@ -45,33 +45,39 @@ def get_model_definition(img_height, img_width, in_channels, out_channels):
         'padding': 'same',
     }
     timesteps = 13
-    abecedary_length = 37
+    abecedary_len = 37
     k_size = (3,) * 2
     h_dim = 10
     h1 = Conv2D(h_dim, kernel_size=k_size, **kwargs_conv2d)(x)
     h1 = Conv2D(h_dim, kernel_size=k_size, **kwargs_conv2d)(h1)
+    h2 = MaxPooling2D((2, 2))(h1)
+    h2 = Conv2D(h_dim * 2, kernel_size=k_size, **kwargs_conv2d)(h2)
+    h2 = Conv2D(h_dim * 2, kernel_size=k_size, **kwargs_conv2d)(h2)
 
     def position_f(pos):
-        pos = np.identity(abecedary_length)[pos]
-        pos = np.reshape(pos, (-1, abecedary_length))
+        pos = np.identity(abecedary_len)[pos]
+        pos = np.reshape(pos, (-1, abecedary_len))
         pos = tf.constant(pos, dtype='float32')
         return pos
 
     x2_f = compose_fs((
         Conv2D(h_dim, kernel_size=k_size, **kwargs_conv2d),
         Conv2D(2, kernel_size=k_size, **kwargs_conv2d),
-        tf.keras.layers.Flatten()
+        MaxPooling2D((2, 2)),
+        Conv2D(h_dim * 2, kernel_size=k_size, **kwargs_conv2d),
+        Conv2D(h_dim * 2, kernel_size=k_size, **kwargs_conv2d),
+        tf.keras.layers.Flatten(),
     ))
     attention_f = compose_fs((
-        tf.keras.layers.Dense(h1.shape[1] * h1.shape[2] * 2),
-        tf.keras.layers.Reshape((h1.shape[1], h1.shape[2], 2)),
+        tf.keras.layers.Dense(h2.shape[1] * h2.shape[2] * 2),
+        tf.keras.layers.Reshape((h2.shape[1], h2.shape[2], 2)),
         tf.keras.layers.Softmax(axis=-1),
     ))
     glimpse_f = compose_fs((
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(out_channels),
         tf.keras.layers.Softmax(axis=1),
-        tf.keras.layers.Reshape((-1, 1, 1, abecedary_length)),
+        tf.keras.layers.Reshape((-1, 1, 1, abecedary_len)),
     ))
     outputs = []
     for it in range(timesteps):
@@ -80,15 +86,15 @@ def get_model_definition(img_height, img_width, in_channels, out_channels):
         x2 = tf.concat([x2, position], axis=-1)
         attention = attention_f(x2)
         attention = attention[:, :, :, 0:1]
-        attention = tf.tile(attention, multiples=[1, 1, 1, h1.shape[-1]])
-        glimpse = tf.concat([h1, attention * h1], axis=-1)
+        attention = tf.tile(attention, multiples=[1, 1, 1, h2.shape[-1]])
+        glimpse = tf.concat([h2, attention * h2], axis=-1)
         glimpse = glimpse_f(glimpse)
         outputs.append(glimpse)
     outputs = tf.concat(outputs, axis=3)
-    outputs = tf.keras.layers.Reshape((-1, timesteps, abecedary_length))(outputs)
+    outputs = tf.keras.layers.Reshape((-1, timesteps, abecedary_len))(outputs)
     print(outputs.shape)
     # Model compilation
-    model = Model(inputs=[inputs], outputs=[outputs])
+    model = Model(inputs=[input_image], outputs=[outputs])
     model.compile(
         optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
     )
